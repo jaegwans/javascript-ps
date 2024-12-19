c368e274910b1084917f7f7c35cb6f1a76f40bc2
김재관 <50818389+jaegwans@users.noreply.github.com>
2024-12-19 21:06:23 +0900

    [Silver III] Title: 주유소, Time: 288 ms, Memory: 43840 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -2,11 +2,11 @@ const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 const N = parseInt(input[0], 10); // 도시의 수
-const distances = input[1].split(' ').map(Number); // 도시 간 거리
-const prices = input[2].split(' ').map(Number); // 주유소의 리터당 가격
+const distances = input[1].split(' ').map(BigInt); // 도시 간 거리
+const prices = input[2].split(' ').map(BigInt); // 주유소의 리터당 가격
 
 let minPrice = prices[0]; // 첫 도시의 주유 가격
-let totalCost = 0 // 총 비용
+let totalCost = BigInt(0) // 총 비용
 
 for (let i = 0; i < N - 1; i++) {
     totalCost += minPrice * distances[i]; // 현재 최소 가격으로 이동 거리 주유
