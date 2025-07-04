725e85d9cf32870f3699be0f1ccdc477c2029a82
김재관 <50818389+jaegwans@users.noreply.github.com>
2025-07-04 12:44:18 +0900

    [level 2] Title: 행렬의 곱셈, Time: 23.11 ms, Memory: 39.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.cpp
@@ -0,0 +1,9 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
+    vector<vector<int>> answer;
+    return answer;
+}
